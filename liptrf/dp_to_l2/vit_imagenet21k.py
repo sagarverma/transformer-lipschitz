e29fac989ee7065b1f5d4d220afb7d9d1fa70d66
Sagar <sagar15056@iiitd.ac.in>
2022-05-15 17:06:46 +0200

    vit_imagenet1k adapt

--- liptrf/dp_to_l2/vit_imagenet21k.py
+++ liptrf/dp_to_l2/vit_imagenet21k.py
@@ -1,16 +1,25 @@
 import os 
 import argparse 
-import pickle as pkl 
-import numpy as np 
+import io 
+from PIL import Image
+import webdataset as wds
 
 import torch 
+import timm
 import torch.nn as nn 
 import torch.nn.functional as F 
 import torch.optim as optim 
 from torchvision import datasets, transforms
 
-from models.vit import ViT, L2Attention
+from liptrf.models.vit import L2Attention
+ 
 
+class Byte2Image(object):
+    def __call__(self, sample):
+        return Image.open(io.BytesIO(sample))
+
+def identity(x):
+    return x
 
 attention_io = {}
 def get_activation(name):
@@ -24,13 +33,16 @@ def train(args, dp_model, l2_model, student_l2_models, device, train_loader,
     student_l2_models = [student_l2_model.train() for student_l2_model in student_l2_models]
     dp_model.eval()
 
+    samples = 0
     student_l2_train_loss = [0] * len(student_l2_models)
     for batch_idx, (data, target) in enumerate(train_loader):
-        data, target = data.to(device), target.to(device)
-        output = dp_model(data)
+        with torch.no_grad():
+            data, target = data.to(device), target.to(device)
+            output = dp_model(data)
+            samples += data.shape[0]
 
         [student_l2_optim.zero_grad() for student_l2_optim in student_l2_optims]
-        for i in range(args.layers):
+        for i in range(12):
             teacher_l2_data = attention_io[f"a{i}"]["in"]
             teacher_l2_target = attention_io[f"a{i}"]["out"]
             student_l2_output = student_l2_models[i](teacher_l2_data)
@@ -39,13 +51,13 @@ def train(args, dp_model, l2_model, student_l2_models, device, train_loader,
             student_l2_train_loss[i] += student_l2_loss.item()
             student_l2_optims[i].step()
 
-    for i in range(args.layers):
-        l2_model.transformer.layers[i][0].fn = student_l2_models[i]
+    for i in range(12):
+        l2_model.blocks[i].attn = student_l2_models[i]
 
-    student_l2_train_loss = [student_l2_loss /len(train_loader.dataset) for student_l2_loss in student_l2_train_loss]
+    student_l2_train_loss = [student_l2_loss / samples for student_l2_loss in student_l2_train_loss]
 
     print(f"Epoch: {epoch}, Train set: Average losses")
-    for i in range(args.layers):
+    for i in range(12):
         print(f"Attention {i}: {student_l2_train_loss[i]}")
 
 
@@ -53,17 +65,18 @@ def test(args, model, device, test_loader, criterion):
     model.eval()
     test_loss = 0
     correct = 0
+    test_samples = 0
     with torch.no_grad():
         for data, target in test_loader:
             data, target = data.to(device), target.to(device)
+            test_samples += data.shape[0]
             output = model(data)
             test_loss += criterion(output, target).item()  # sum up batch loss
             pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log_probability
             correct += pred.eq(target.view_as(pred)).sum().item()
             torch.cuda.empty_cache()
 
-    test_loss /= len(test_loader.dataset)
-    test_samples = len(test_loader.dataset)
+    test_loss /= test_samples
 
     print(f"Test set: Average loss: {test_loss:.4f}, " +
           f"Accuracy: {correct}/{test_samples} " +
@@ -75,10 +88,8 @@ def test(args, model, device, test_loader, criterion):
 def main():
     # Training settings
     parser = argparse.ArgumentParser(description='PyTorch MNIST ViT')
-    parser.add_argument('--task', type=str, default='train',
-                        help='train/retrain/extract/test')
-
-    parser.add_argument('--layers', type=int, default=1)
+    parser.add_argument('--task', type=str, default='adapt',
+                        help='adapt/test')
     
     parser.add_argument('--batch_size', type=int, default=64, metavar='N',
                         help='input batch size for training (default: 64)')
@@ -100,44 +111,67 @@ def main():
                         help='data path of MNIST')
     parser.add_argument('--weight_path', type=str, required=True, 
                         help='weight path save directory')
-    parser.add_argument('--dp_weight_path', type=str, required=True,
-                        help='weight path of ViT trained with DP attention')
 
     args = parser.parse_args()
 
     torch.manual_seed(args.seed)
     device = torch.device(args.gpu)
 
-    transform=transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize((0.1307,), (0.3081,))
-        ])
-    dataset1 = datasets.MNIST(args.data_path, train=True, download=True,
-                       transform=transform)
-    dataset2 = datasets.MNIST(args.data_path, train=False,
-                       transform=transform)
-    train_loader = torch.utils.data.DataLoader(dataset1, batch_size=args.batch_size, 
-                                                num_workers=args.num_workers, shuffle=True)
-    test_loader = torch.utils.data.DataLoader(dataset2, batch_size=args.batch_size, 
-                                                num_workers=args.num_workers, shuffle=False)
+    train_transform = transforms.Compose(
+                [   Byte2Image(),
+                    transforms.RandomResizedCrop(224),
+                    transforms.RandomHorizontalFlip(),
+                    transforms.ToTensor(),
+                    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+                ]
+            )
+    val_transform = transforms.Compose(
+                [   Byte2Image(),
+                    transforms.Resize(256),
+                    transforms.CenterCrop(224),
+                    transforms.ToTensor(),
+                    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+                ]
+            )
+    
+    train_dataset = (wds.WebDataset(os.path.join(args.data_path, 'train/train-{0000..1878}.tar'))
+            .shuffle(True)
+            .decode("pil")
+            .to_tuple("x.img.pil y.cls")
+            .map_tuple(train_transform, identity)
+            .batched(args.batch_size, partial=False))
+    val_dataset = (
+            wds.WebDataset(os.path.join(args.data_path, 'val/val-{00..48}.tar'))
+            .shuffle(False)
+            .decode("pil")
+            .to_tuple("x.img.pil y.cls")
+            .map_tuple(val_transform, identity)
+            .batched(args.batch_size, partial=False)
+        )
+    train_loader =  wds.WebLoader(
+            train_dataset,
+            batch_size=None,
+            shuffle=False,
+            num_workers=args.num_workers,
+        )
+    test_loader =  wds.WebLoader(
+            val_dataset,
+            batch_size=None,
+            shuffle=False,
+            num_workers=args.num_workers,
+        )
 
-    l2_model = ViT(image_size=28, patch_size=7, num_classes=10, channels=1,
-                dim=128, depth=args.layers, heads=8, mlp_ratio=4, attention_type='L2', lmbda=1).cuda()
-    dp_model = ViT(image_size=28, patch_size=7, num_classes=10, channels=1,
-                dim=128, depth=args.layers, heads=8, mlp_ratio=4, attention_type='DP', lmbda=1).cuda()
-    weight = torch.load(args.dp_weight_path)
-    dp_model.load_state_dict(weight)
+    l2_model = timm.create_model('vit_base_patch16_224', pretrained=True).to(device)
+    dp_model = timm.create_model('vit_base_patch16_224', pretrained=True).to(device)
     dp_model.eval()
-
-    l2_model.load_state_dict(weight, strict=False)
 
     student_l2_criterion = nn.MSELoss()
     student_l2_optims = []
     student_l2_schedulers = []
     student_l2_models = []
-    for i in range(args.layers):
-        dp_model.transformer.layers[i][0].fn.register_forward_hook(get_activation(f'a{i}'))
-        student_l2_model = L2Attention(dim=128, heads=8).cuda()
+    for i in range(12):
+        dp_model.blocks[i].attn.register_forward_hook(get_activation(f'a{i}'))
+        student_l2_model = L2Attention(dim=768, heads=12).to(device)
 
         if args.opt == 'adam': 
             student_l2_optimizer = optim.Adam(student_l2_model.parameters(), lr=args.lr)
@@ -154,19 +188,9 @@ def main():
         student_l2_schedulers.append(student_l2_scheduler)
 
     criterion = nn.CrossEntropyLoss()
-    # if args.opt == 'adam': 
-    #     optimizer = optim.Adam(l2_model.parameters(), lr=args.lr)
-    # elif args.opt == 'sgd': 
-    #     optimizer = optim.SGD(l2_model.parameters(), lr=args.lr, 
-    #                     momentum=0.9,
-    #                     weight_decay=0.0) 
-    # scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer,
-    #                                                  milestones=[50, 60,
-    #                                                              70, 80],
-    #                                                  gamma=0.2)
 
-    if args.task == 'train':
-        weight_path = os.path.join(args.weight_path, f"vit_mnist_layers-{args.layers}_att-L2_adaptedFrom_DP.pt")
+    if args.task == 'adapt':
+        weight_path = os.path.join(args.weight_path, f"vit_base_patch16_224_att-L2_adaptedFrom_DP.pt")
 
         if not os.path.exists(args.weight_path):
             os.mkdir(args.weight_path)
@@ -183,10 +207,10 @@ def main():
             if acc > best_acc:
                 torch.save(l2_model.state_dict(), weight_path)
             
-    # if args.task == 'test':
-    #     weight = torch.load(args.weight_path)
-    #     model.load_state_dict(weight)
-    #     test(args, model, device, test_loader, criterion)
+    if args.task == 'test':
+        weight = torch.load(args.weight_path)
+        l2_model.load_state_dict(weight)
+        test(args, l2_model, device, test_loader, criterion)
 
 if __name__ == '__main__':
     main()

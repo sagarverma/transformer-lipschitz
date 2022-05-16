c82c06e0b21c9ab3f78cd3db31b4c6a97c36265a
sagarverma <sagar15056@iiitd.ac.in>
2022-05-16 09:00:03 +0200

    ddp stuff

--- liptrf/dp_to_l2/vit_imagenet1k.py
+++ liptrf/dp_to_l2/vit_imagenet1k.py
@@ -7,15 +7,17 @@ import webdataset as wds
 import torch 
 import timm
 import torch.nn as nn 
+import torch.backend.cudnn as cudnn
 import torch.nn.functional as F 
 import torch.optim as optim
 import torch.distributed as dist 
+import torch.multiprocessing as mp
 from torchvision import datasets, transforms
 from torch.nn.parallel import DistributedDataParallel as DDP 
 
 from liptrf.models.vit import L2Attention
  
- 
+
 def setup(rank, world_size):
     os.environ['MASTER_ADDR'] = 'localhost'
     os.environ['MASTER_ROOT'] = '12355'
@@ -28,6 +30,52 @@ def cleanup():
 class Byte2Image(object):
     def __call__(self, sample):
         return Image.open(io.BytesIO(sample))
+
+def get_dataset(args):
+    world_size = dist.get_world_size()
+    train_transform = transforms.Compose(
+                [   Byte2Image(),
+                    transforms.RandomResizedCrop(224),
+                    transforms.RandomHorizontalFlip(),
+                    transforms.ToTensor(),
+                    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+                ]
+            )
+    val_transform = transforms.Compose(
+                [   Byte2Image(),
+                    transforms.Resize(256),
+                    transforms.CenterCrop(224),
+                    transforms.ToTensor(),
+                    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+                ]
+            )
+    
+    train_dataset = (wds.WebDataset(os.path.join(args.data_path, 'train/train-{0000..1878}.tar'))
+            .shuffle(True)
+            .decode("pil")
+            .to_tuple("x.img.pil y.cls")
+            .map_tuple(train_transform, identity)
+            .batched(args.batch_size, partial=False))
+    val_dataset = (
+            wds.WebDataset(os.path.join(args.data_path, 'val/val-{00..48}.tar'))
+            .shuffle(False)
+            .decode("pil")
+            .to_tuple("x.img.pil y.cls")
+            .map_tuple(val_transform, identity)
+            .batched(args.batch_size, partial=False)
+        )
+    train_loader =  wds.WebLoader(
+            train_dataset,
+            batch_size=None,
+            shuffle=False,
+            num_workers=args.num_workers,
+        )
+    test_loader =  wds.WebLoader(
+            val_dataset,
+            batch_size=None,
+            shuffle=False,
+            num_workers=args.num_workers,
+        )
 
 def identity(x):
     return x
@@ -129,49 +177,7 @@ def main():
     torch.manual_seed(args.seed)
     device = torch.device(args.gpu)
 
-    train_transform = transforms.Compose(
-                [   Byte2Image(),
-                    transforms.RandomResizedCrop(224),
-                    transforms.RandomHorizontalFlip(),
-                    transforms.ToTensor(),
-                    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-                ]
-            )
-    val_transform = transforms.Compose(
-                [   Byte2Image(),
-                    transforms.Resize(256),
-                    transforms.CenterCrop(224),
-                    transforms.ToTensor(),
-                    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-                ]
-            )
     
-    train_dataset = (wds.WebDataset(os.path.join(args.data_path, 'train/train-{0000..1878}.tar'))
-            .shuffle(True)
-            .decode("pil")
-            .to_tuple("x.img.pil y.cls")
-            .map_tuple(train_transform, identity)
-            .batched(args.batch_size, partial=False))
-    val_dataset = (
-            wds.WebDataset(os.path.join(args.data_path, 'val/val-{00..48}.tar'))
-            .shuffle(False)
-            .decode("pil")
-            .to_tuple("x.img.pil y.cls")
-            .map_tuple(val_transform, identity)
-            .batched(args.batch_size, partial=False)
-        )
-    train_loader =  wds.WebLoader(
-            train_dataset,
-            batch_size=None,
-            shuffle=False,
-            num_workers=args.num_workers,
-        )
-    test_loader =  wds.WebLoader(
-            val_dataset,
-            batch_size=None,
-            shuffle=False,
-            num_workers=args.num_workers,
-        )
 
     l2_model = timm.create_model('vit_base_patch16_224', pretrained=True).to(device)
     dp_model = timm.create_model('vit_base_patch16_224', pretrained=True).to(device)

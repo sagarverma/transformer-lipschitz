b3cad01e155b6589c65057a9206f6742b71983e4
sagarverma <sagar15056@iiitd.ac.in>
2022-05-18 12:26:31 +0200

    adaptation

--- liptrf/dp_to_l2/vit_cifar10.py
+++ liptrf/dp_to_l2/vit_cifar10.py
@@ -2,6 +2,7 @@ import os
 import argparse 
 import pickle as pkl 
 import numpy as np 
+import csv 
 
 import timm
 import torch 
@@ -15,41 +16,31 @@ from liptrf.models.layers import LinearX
 from liptrf.models.timm_vit import VisionTransformer as ViT
 
 
-attention_io = {}
-def get_activation(name):
-    def hook(model, input, output):
-        attention_io[name] = {"in": input[0].detach(),
-                              "out": output.detach()}
-    return hook 
-
-def train(args, dp_model, l2_model, student_l2_models, device, train_loader,
-          student_l2_optims, epoch, criterion, finetune=False):
-    student_l2_models = [student_l2_model.train() for student_l2_model in student_l2_models]
-    dp_model.eval()
-
-    student_l2_train_loss = [0] * len(student_l2_models)
+def train(args, model, device, train_loader,
+          optimizer, epoch, criterion, finetune=False):
+    model.train()
+    train_loss = 0
+    correct = 0
     for batch_idx, (data, target) in enumerate(train_loader):
         data, target = data.to(device), target.to(device)
-        output = dp_model(data)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = criterion(output, target)
+        loss.backward()
+        train_loss += loss.item()
+        pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log_probability
+        correct += pred.eq(target.view_as(pred)).sum().item()
+        optimizer.step()
 
-        [student_l2_optim.zero_grad() for student_l2_optim in student_l2_optims]
-        for i in range(args.layers):
-            teacher_l2_data = attention_io[f"a{i}"]["in"]
-            teacher_l2_target = attention_io[f"a{i}"]["out"]
-            student_l2_output = student_l2_models[i](teacher_l2_data)
-            student_l2_loss = criterion(student_l2_output, teacher_l2_target)
-            student_l2_loss.backward()
-            student_l2_train_loss[i] += student_l2_loss.item()
-            student_l2_optims[i].step()
+        torch.cuda.empty_cache()
 
-    for i in range(args.layers):
-        l2_model.blocks[i].attn = student_l2_models[i]
+    train_loss /= len(train_loader.dataset)
+    train_samples = len(train_loader.dataset)
 
-    student_l2_train_loss = [student_l2_loss /len(train_loader.dataset) for student_l2_loss in student_l2_train_loss]
-
-    print(f"Epoch: {epoch}, Train set: Average losses")
-    for i in range(args.layers):
-        print(f"Attention {i}: {student_l2_train_loss[i]}")
+    print(f"Epoch: {epoch}, Train set: Average loss: {train_loss:.4f}, " +
+          f"Accuracy: {correct}/{train_samples} " +
+          f"({100.*correct/train_samples:.0f}%), " +
+          f"Error: {(train_samples-correct)/train_samples * 100:.2f}%")
 
 
 def test(args, model, device, test_loader, criterion):
@@ -71,17 +62,16 @@ def test(args, model, device, test_loader, criterion):
     print(f"Test set: Average loss: {test_loss:.4f}, " +
           f"Accuracy: {correct}/{test_samples} " +
           f"({100.*correct/test_samples:.0f}%), " +
-          f"Error: {(test_samples-correct)/test_samples * 100:.2f}% " +
-          f"Lipschitz {model.lipschitz().item():4f} \n")
-    return 100.*correct/test_samples
+          f"Error: {(test_samples-correct)/test_samples * 100:.2f}%\n")
+    return 100.*correct/test_samples, test_loss
 
 def main():
     # Training settings
-    parser = argparse.ArgumentParser(description='PyTorch MNIST ViT')
+    parser = argparse.ArgumentParser(description='PyTorch CIFAR10 ViT')
     parser.add_argument('--task', type=str, default='train',
                         help='train/retrain/extract/test')
 
-    parser.add_argument('--layers', type=int, default=1)
+    parser.add_argument('--layers', type=int, default=12)
     
     parser.add_argument('--batch_size', type=int, default=64, metavar='N',
                         help='input batch size for training (default: 64)')
@@ -93,6 +83,8 @@ def main():
                         help='adam/sgd')
     parser.add_argument('--num_workers', type=int, default=4,
                         help='Number of cores to use')
+    parser.add_argument('--cos', action='store_false', 
+                        help='Train with cosine annealing scheduling')
 
     parser.add_argument('--gpu', type=int, default=0,
                         help='gpu to use')
@@ -100,7 +92,7 @@ def main():
                         help='random seed (default: 1)')
 
     parser.add_argument('--data_path', type=str, required=True,
-                        help='data path of MNIST')
+                        help='data path of CIFAR10')
     parser.add_argument('--dp_weight_path', type=str, required=True,
                         help='weight path of ViT trained with DP attention')
 
@@ -133,57 +125,52 @@ def main():
     test_loader = torch.utils.data.DataLoader(
         testset, batch_size=args.batch_size, shuffle=False, num_workers=args.num_workers)
 
-    l2_model =  ViT(patch_size=16, embed_dim=192, depth=12, num_heads=3, lmbda=1, num_classes=10)
-    dp_model = timm.create_model('vit_tiny_patch16_224', pretrained=False, num_classes=10)
+    model =  ViT(patch_size=16, embed_dim=192, depth=12, num_heads=3, lmbda=1, num_classes=10)
     args.layers = 12
     weight = torch.load(args.dp_weight_path)
-    dp_model.load_state_dict(weight)
-    dp_model.eval()
-
-    l2_model.load_state_dict(weight, strict=False)
-
-    student_l2_criterion = nn.MSELoss()
-    student_l2_optims = []
-    student_l2_schedulers = []
-    student_l2_models = []
-    for i in range(args.layers):
-        dp_model.blocks[i].attn.register_forward_hook(get_activation(f'a{i}'))
-        student_l2_model = L2Attention(dim=192, heads=3).cuda()
-
-        if args.opt == 'adam': 
-            student_l2_optimizer = optim.Adam(student_l2_model.parameters(), lr=args.lr)
-        elif args.opt == 'sgd': 
-            student_l2_optimizer = optim.SGD(student_l2_model.parameters(), lr=args.lr, 
-                            momentum=0.9,
-                            weight_decay=0.0) 
-        student_l2_scheduler = torch.optim.lr_scheduler.MultiStepLR(student_l2_optimizer,
-                                                        milestones=[50, 60,
-                                                                    70, 80],
-                                                        gamma=0.2)
-        student_l2_models.append(student_l2_model)
-        student_l2_optims.append(student_l2_optimizer)
-        student_l2_schedulers.append(student_l2_scheduler)
-
+    model.load_state_dict(weight, strict=False)
     criterion = nn.CrossEntropyLoss()
 
+    for i in range(args.layers):
+        model.blocks[i].attn = L2Attention(dim=192, heads=3, dropout=0.1)
+
+    model = model.to(device)
+    
+    if args.opt == 'adam': 
+        optimizer = optim.Adam(model.parameters(), lr=args.lr,
+                               betas=(0.9, 0.999), weight_decay=5e-5)
+    elif args.opt == 'sgd': 
+        optimizer = optim.SGD(model.parameters(), lr=args.lr, 
+                        momentum=0.9,
+                        weight_decay=0.0) 
+    # use cosine or reduce LR on Plateau scheduling
+    if not args.cos:
+        scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min', 
+                                                        patience=3, verbose=True, 
+                                                        min_lr=1e-3*1e-5, factor=0.1)
+    else:
+        scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=args.epochs, 
+                                                         eta_min=1e-5)
     if args.task == 'train':
         weight_path = os.path.join(args.dp_weight_path.replace('.pt', '_L2-Adapted.pt'))
-
-        if not os.path.exists(args.weight_path):
-            os.mkdir(args.weight_path)
+        fout = open(weight_path.replace('.pt', '.csv').replace('weights', 'logs'), 'w')
+        w = csv.writer(fout)
 
         best_acc = -1
         for epoch in range(1, args.epochs + 1):
-            train(args, dp_model, l2_model, student_l2_models, device, train_loader,
-                  student_l2_optims, epoch, student_l2_criterion, False)
-            acc = test(args, l2_model, device, test_loader, criterion)
-            
-            for student_l2_scheduler in student_l2_schedulers:
-                student_l2_scheduler.step()
-
+            train(args, model, device, train_loader,
+                  optimizer, epoch, criterion, False)
+            acc, loss = test(args, model, device, test_loader, criterion)
+            w.writerow([epoch, acc, loss, 'NaN'])
+        
+            if args.cos:
+                scheduler.step(epoch-1)
+            else:
+                scheduler.step(loss)
+        
             if acc > best_acc:
-                torch.save(l2_model.state_dict(), weight_path)
+                best_acc = acc
+                torch.save(model.state_dict(), weight_path)
             
-
 if __name__ == '__main__':
     main()

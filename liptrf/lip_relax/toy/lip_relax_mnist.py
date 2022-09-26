714d1a932203a3df6d3009d15ff84657a5144ad1
Sagar <sagar15056@iiitd.ac.in>
2022-09-26 12:08:19 +0200

    fix

--- liptrf/lip_relax/toy/lip_relax_mnist.py
+++ liptrf/lip_relax/toy/lip_relax_mnist.py
@@ -137,6 +137,7 @@ def process_layers(layers, model, train_loader, test_loader,
         if verified >= verified_best:
             verified_best = verified
             verified_best_state = model.state_dict()
+            pgd = evaluate_pgd(test_loader, model, epsilon=1.58, niter=100, alpha=1.58/4)
             weight_path = args.weight_path.replace('.pt', f"_lc_alpha-{args.lc_alpha}_eta-{args.eta}_lc_gamma-{args.lc_gamma}_lr-{args.lr}.pt")
             out_dict = {"weights": model.state_dict(), "clean": clean, "lip": lip, "pgd": pgd, "verified": verified}
             torch.save(out_dict, weight_path)
@@ -144,7 +145,6 @@ def process_layers(layers, model, train_loader, test_loader,
     model.load_sate_dict(verified_best_state)
     test(args, model, device, test_loader, criterion)
     pgd = evaluate_pgd(test_loader, model, epsilon=1.58, niter=100, alpha=1.58/4)
-            
 
 def print_nonzeros(model):
     nonzero = total = 0 

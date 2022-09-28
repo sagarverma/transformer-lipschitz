88bbbe5ff3f89170c7d84873fdd6ee2a6fa46f83
sagarverma <sagar15056@iiitd.ac.in>
2022-09-28 15:07:50 +0200

    Fix

--- liptrf/scratch/toy/train_mnist.py
+++ liptrf/scratch/toy/train_mnist.py
@@ -194,7 +194,7 @@ def main():
         model.load_state_dict(best_state)
         model.eval()
         test(args, model, device, test_loader, criterion)
-        evaluate_pgd(test_loader, model, epsilon=1.58, niter=100, alpha=1.58/4)
+        evaluate_pgd(test_loader, model, epsilon=1.58, niter=100, alpha=1.58/4, device=device)
 
 
 

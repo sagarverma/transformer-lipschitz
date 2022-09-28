dcee236d2322b127d84d75f368cee184fd81bd3e
sagarverma <sagar15056@iiitd.ac.in>
2022-09-28 14:54:17 +0200

    fix

--- liptrf/scratch/moderates/train_cifar10.py
+++ liptrf/scratch/moderates/train_cifar10.py
@@ -132,12 +132,12 @@ def main():
         transforms.RandomCrop(32, padding=4),
         transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
-        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+        transforms.Normalize((0.485, 0.456, 0.406), (0.225, 0.225, 0.225)),
     ])
 
     transform_test = transforms.Compose([
         transforms.ToTensor(),
-        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+        transforms.Normalize((0.485, 0.456, 0.406), (0.225, 0.225, 0.225)),
     ])
 
     trainset = datasets.CIFAR10(

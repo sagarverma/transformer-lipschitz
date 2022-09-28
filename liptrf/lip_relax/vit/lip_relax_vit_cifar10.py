90b681ee0ad509d0ff6466476f433f31aeba3ae4
sagarverma <sagar15056@iiitd.ac.in>
2022-09-29 00:41:28 +0200

    Fix

--- liptrf/lip_relax/vit/lip_relax_vit_cifar10.py
+++ liptrf/lip_relax/vit/lip_relax_vit_cifar10.py
@@ -102,8 +102,8 @@ def process_layers(layers, model, train_loader, test_loader,
         for lipr_epoch in range(args.lipr_epochs):
             layer.prox()
 
-            if layer.lipschitz() <= 1:
-                break
+            # if layer.lipschitz() <= 1:
+            #     break
         
             for proj_epoch in tqdm.tqdm(range(args.proj_epochs)):
                 layer.proj_weight_old = layer.proj_weight.clone().detach()
@@ -127,8 +127,8 @@ def process_layers(layers, model, train_loader, test_loader,
             print (f"Prox {lipr_epoch} Proj {proj_epoch} Layer Lip {layer.lipschitz().item():.2f}")
             test(args, model, device, test_loader, criterion)
             layer.weight = nn.Parameter(old_weight)
-            if layer.lc <= 1:
-                break 
+            # if layer.lc <= 1:
+            #     break 
 
             if torch.linalg.norm(layer.weight_t - layer.weight_old) < args.lipr_prec * torch.norm(layer.weight_t):
                 break
